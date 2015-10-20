5d79a89e52fefb5590f91a8d1ece041230a2897b
jessicalc <jessica@crayoncollective.com>
2015-10-19 21:27:41 -0700

    (feat) Create functions to seed DB with events and tags
    TODO: add edges to tags.
    Also fixed package.json file.

---
+++ dateServer/data-helpers/seedAll.js
@@ -0,0 +1,18 @@
+var tags = require('./tags');
+var events = require('./events');
+
+tags.seedTags(function(err, results) {
+	if (err) {
+    console.log("There was an error seeding tags in the DB.", err);
+  } else {
+    console.log("Great job, the tag seeding worked!");
+  }
+});
+
+events.seedEvents(function(err, results) {
+  if(err) {
+    console.log("There was an error seeding events in the DB.", err);
+  } else {
+    console.log("The events got seeded!");
+  }
+})
